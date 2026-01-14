9f8958ccf220d44092b7e274b30e7b5b9063e1d1
HanielUlises <hanielulises2003@gmail.com>
2026-01-13 21:36:14 -0600

    Online assembler with linker script

---
+++ online_assembler/oa.asm
@@ -0,0 +1,12 @@
+.global _boot
+.text
+
+_boot:
+    addi x1, x0, 1000
+    addi x2, x1, 2000
+    addi x3, x2, -1000
+    addi x4, x3, -2000
+    addi x5, x4, 1000
+
+    la x6, variable
+    addi x6, x6, 4
