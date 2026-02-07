bf02943f3901857e36d78674ab98f0ff3598258f
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2026-02-06 22:03:57 -0600

    Verilog module

--- Test/my_design.v
+++ Test/my_design.v
@@ -7,15 +7,14 @@ module my_design (
 );
 
     reg [31:0] d_out_pre;
-    always @(posedge clk or negedge reset_n) begin
-        if(!reset_n) begin
-            d_out_pre <= 'h0;
-        end
-        else begin
-            d_out_pre <= d_in_1;
-        end
 
-        assign d_out = d_in_2 ^ d_out_pre;
+    always @(posedge clk or negedge reset_n) begin
+        if (!reset_n)
+            d_out_pre <= 32'h0;
+        else
+            d_out_pre <= d_in_1;
     end
+
+    assign d_out = d_in_2 ^ d_out_pre;
 
 endmodule
